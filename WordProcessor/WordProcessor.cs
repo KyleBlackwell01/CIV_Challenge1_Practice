5ddbf124560135f5926dba91ed06c43e2cec4e20
KYLE BLACKWELL <102472889@student.swin.edu.au>
2019-08-28 08:14:13 +1000

    Added baseline parts for class library

--- WordProcessor/WordProcessor.cs
+++ WordProcessor/WordProcessor.cs
@@ -10,6 +10,32 @@ namespace WordProcessor
         public string AlternateCase(string input)
         {
             throw new NotImplementedException();
+
+        }
+
+        public string ShiftWord(string input, int shiftAmt)
+        {
+            throw new NotImplementedException();
+        }
+
+        public char MostCommonLetter(string input)
+        {
+            throw new NotImplementedException();
+        }
+
+        public int AmtOfDifferentChars(string input)
+        {
+            throw new NotImplementedException();
+        }
+
+        public int findPattern(string input, string pattern)
+        {
+            throw new NotImplementedException();
+        }
+
+        public string findLongestPattern(string input1, string input2)
+        {
+            throw new NotImplementedException();
         }
 
     }
