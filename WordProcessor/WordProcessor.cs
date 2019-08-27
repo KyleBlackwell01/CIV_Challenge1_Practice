bbd0f7a32343663bb207745894219c2e2b2b21f6
KYLE BLACKWELL <102472889@student.swin.edu.au>
2019-08-28 08:10:50 +1000

    Added Class Library

---
+++ WordProcessor/WordProcessor.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace WordProcessor
+{
+    public class WordProcessor
+    {
+
+        public string AlternateCase(string input)
+        {
+            throw new NotImplementedException();
+        }
+
+    }
+}
